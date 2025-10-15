461cab85620e7cd96155f96299d512ece6203c02
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2025-10-15 15:16:18 +0400

    Add functionality to change grid size

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const container = document.querySelector("div");
-for (let i = 0; i < 256; i++) {
+const button = document.querySelector("button");
+const containerWidth = 640;
+let pixelsPerSide = 16;
+let pixelSize = containerWidth / pixelsPerSide;
+for (let i = 0; i < pixelsPerSide ** 2; i++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
+    pixel.style.width = `${pixelSize}px`;
+    pixel.style.height = `${pixelSize}px`;
     container.appendChild(pixel);
 
     pixel.addEventListener("mouseenter", () => {
         pixel.classList.toggle("pixel-hover");
     });
 }
+function changeGrid () {
+    pixelsPerSide = prompt("Enter the number of squares per side (100 max):", "16");
+    const pixelsToRemove= document.querySelectorAll(".pixel");
+    pixelsToRemove.forEach(element => {
+        element.remove();
+    });
+    pixelSize = containerWidth / pixelsPerSide;
+    for (let i = 0; i < pixelsPerSide ** 2; i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.style.width = `${pixelSize}px`;
+        pixel.style.height = `${pixelSize}px`;
+        container.appendChild(pixel);
+
+        pixel.addEventListener("mouseenter", () => {
+        pixel.classList.toggle("pixel-hover");
+        });
+    }
+}
+button.onclick = changeGrid;
